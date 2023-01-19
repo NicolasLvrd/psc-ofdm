8c034dbee1396cfe715884a2252133dbac080d06
Druffle <72805514+Druffle@users.noreply.github.com>
2023-01-19 11:48:30 +0100

    Add files via upload

--- script.m
+++ script.m
@@ -4,10 +4,12 @@ load samples.mat
 % Définir la taille d'un demi-symbole L
 L = 32;
 
-samples = samples1;
+samples = samples3;
 t = length(samples);
 T = 64*50e-9;
 start = 1;
+Ng = 3;
+ds = 1;
 
 % Initialiser à zéro
 M = zeros(1, uint64((t/(2*L))+1));
@@ -17,10 +19,9 @@ M1 = zeros(1, uint64((t/(2*L))+2*L+1));
 Mf = zeros(1, uint64((t/(2*L))+1));
 Rf = zeros(1, uint64((t/(2*L))+1));
 phi = zeros(1, uint64((t/(2*L))+1));
-offset = zeros(1, uint64((t/(2*L))+1));
+offset = zeros(1, uint64((t/(4000*L))+1));
 compt = 1;
-res=1;
-deltaf = 0;
+res=zeros(1, uint64((t/(2*L))+1));
 
 % Transposer les échantillons pour les mettre en ligne
 samples = samples.';
@@ -33,7 +34,7 @@ for d = start:t-2*L
     P(compt) = sum(samples(d+L+1:d+L+L) .* conj(samples(d+1:d+L)));
     % normaliser la corrélation par l'énergie du demi-symbole
     R(compt) = sum(abs(samples(d+1+L:d+L+L)).^2);
-%%    Rf(compt)=(1/2)*(sum(abs(samples(d:d+63).^2)));
+    Rf(compt)=(1/2)*(sum(abs(samples(d:d+63).^2)));
     % vérifier que corr et energy ne sont pas égaux à zéro
     if P(compt) ~= 0 && R(compt) ~= 0
         M(compt) = abs(P(compt)).^2 ./ R(compt).^2;
@@ -41,28 +42,38 @@ for d = start:t-2*L
         M(compt) = 0;
         disp(['corr = ', num2str(P(compt)), ' energy = ', num2str(R(compt)), ' for d = ', num2str(d), ' taille = ', num2str(t)])
     end
-%%    if P(compt~=0 && Rf(compt ~=0))
-%%         Mf(compt) = abs(P(compt)).^2 ./ Rf(compt).^2;
-%%    else
-%%         Mf(compt) = 0;
-%%    end
-%%    if compt>65
-%%        M1(compt)=(1/(64+1))*sum(Mf(compt-64 : compt));
-%%  
-%%        if M1(compt)>M1(res)
-%%            res = compt;
-%%        end
-   
-%%        deltaf = angle(P(res))/(pi*T);
-%%    end
-    if M(compt)>0.99 && M(compt)<=1
-        phi(compt) = angle(P(compt));
-        offset(compt) = phi(compt)/(pi*T);
+    if P(compt)~=0 && Rf(compt) ~=0
+         Mf(compt) = abs(P(compt)).^2 ./ Rf(compt).^2;
+    else
+         Mf(compt) = 0;
     end
+
+    if d>12 
+        M1(compt)=(1/(Ng+1))*sum(Mf(d-Ng : d));
+        %if M1(compt) > M1(res)
+         %  res = compt;
+        %end
+    end
+    
+    if d > 2000*ds
+        res = 1;
+        for i = (2000*(ds-1))+1:(2000*(ds))+1
+            if M1(i) > M1(res)
+                res = i;
+            end
+        end
+        
+        offset(ds) = angle(P(res))/(pi*T); 
+        ds = ds + 1;
+    end
+    
     compt = compt+1;
 end
 
-test = phi(99949);
+res2 = find(M1 == max(M1));
+deltaf = mean(offset(~isnan(offset)));
+
+test = angle(P(29986));
 test2 = test/(pi*T);
 
 
